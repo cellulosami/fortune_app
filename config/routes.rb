3eb6f956d01cca46972b7997a0bf76436abbe9f3
austin shields <ajshields196@gmail.com>
2021-02-21 12:31:33 -0500

    add fortune path, method, and view

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/fortune_path" => "my_examples#fortune_action"
+  end
 end
